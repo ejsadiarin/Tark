79ae51f06ba8afe19a308adf491f1deedb8db396
ejsadiarin <ejsadiarin@gmail.com>
2023-08-28 23:53:39 +0800

    added Sidebar clean

--- tark-client/src/App.tsx
+++ tark-client/src/App.tsx
@@ -1,25 +1,33 @@
 import { useState } from 'react'
 import { Button } from './components/ui/button'
 import Navbar from './components/Navbar'
+import { Sidebar } from './components/Sidebar'
+import { ThemeProvider } from './components/ui/theme-provider'
+import { ModeToggle } from './components/ui/mode-toggle'
 
 function App() {
   return (
     <>
-      <div className='flex flex-col h-full '>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ModeToggle />
+        <div className='flex h-full '>
 
-        <Navbar />
 
-        {/* Main Dashboard UI */}
-        <div className='flex w-full '>
-          <h1>main</h1>
+          {/* <Navbar /> */}
+          <Sidebar />
+
+          {/* Main Dashboard UI */}
+          <div className='flex w-full '>
+            <h1>main</h1>
+          </div>
+
+
         </div>
-
-
-      </div>
-      {/* Footer */}
-      <div className="bg-[#0f172a] text-white flex justify-center">
-        <h1 className="text-xl">Made by Ej Sadiarin</h1>
-      </div>
+        {/* Footer */}
+        <div className="bg-[#0f172a] text-white flex justify-center">
+          <h1 className="text-xl">Made by Ej Sadiarin</h1>
+        </div>
+      </ThemeProvider>
     </>
   )
 }
